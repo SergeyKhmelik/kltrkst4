53b5d84a42ff6a5ba7829c9a3f602373e34448fd
SergeyKhmelik <sergey.khmelik@gmail.com>
2015-01-31 21:39:00 +0200

    fixed project 3

--- SummaryTask4/src/ua/nure/khmelik/SummaryTask4/web/servlet/LoginServlet.java
+++ SummaryTask4/src/ua/nure/khmelik/SummaryTask4/web/servlet/LoginServlet.java
@@ -22,82 +22,80 @@ import ua.nure.khmelik.SummaryTask4.service.PermissionService;
 
 public class LoginServlet extends HttpServlet {
 
-    private static final long serialVersionUID = -9056876894273126770L;
+	private static final long serialVersionUID = -9056876894273126770L;
 
-    private static final Logger LOGGER = Logger.getLogger(LoginServlet.class);
+	private static final Logger LOGGER = Logger.getLogger(LoginServlet.class);
 
-    private AuthorizationService authorizationService;
-    private PermissionService permissionService;
+	private AuthorizationService authorizationService;
+	private PermissionService permissionService;
 
-    @Override
-    public void init() throws ServletException {
-	super.init();
-	authorizationService = (AuthorizationService) getServletContext()
-		.getAttribute("authorizationService");
-	permissionService = (PermissionService) getServletContext()
-		.getAttribute("permissionService");
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		authorizationService = (AuthorizationService) getServletContext()
+				.getAttribute("authorizationService");
+		permissionService = (PermissionService) getServletContext()
+				.getAttribute("permissionService");
 
-	if (authorizationService == null || permissionService == null) {
-	    LOGGER.error("Could not get services from appcontext");
-	    throw new UnavailableException("Couldn`t get DAO");
-	}
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request,
-	    HttpServletResponse response) throws ServletException, IOException {
-	response.setContentType("text/html");
-
-	String login = request.getParameter("login");
-	String password = request.getParameter("password");
-
-	LOGGER.info("Entered login servlet: login=" + login + " password="
-		+ password);
-
-	if (!validate(login, password)) {
-	    request.setAttribute("loginvalidation",
-		    "Login or password cannot be empty.");
-	    request.getRequestDispatcher("/login")
-		    .forward(request, response);
-	    return;
+		if (authorizationService == null || permissionService == null) {
+			LOGGER.error("Could not get services from appcontext");
+			throw new UnavailableException("Couldn`t get DAO");
+		}
 	}
 
-	User user;
-	ArrayList<Permission> permissions;
-	HttpSession session = request.getSession();
-	try {
-	    user = authorizationService.getUser(login, password);
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
 
-	    if (user == null) {
-		throw new NoSuchUserException();
-	    }
+		String login = request.getParameter("login");
+		String password = request.getParameter("password");
 
-	    permissions = permissionService.getPermissions(user.getIdRole());
-	    session.setAttribute("user", user);
-	    session.setAttribute("permissions", permissions);
+		LOGGER.info("Entered login servlet: login=" + login + " password="
+				+ password);
 
-	    // request.getRequestDispatcher("/mainjsp").forward(request,
-	    // response);
-	    LOGGER.info("User " + user.getLogin() + "(id:" + user.getId()
-		    + ") logged in. ");
-	    response.sendRedirect("main");
+		if (!validate(login, password)) {
+			request.setAttribute("loginvalidation",
+					"Login or password cannot be empty.");
+			request.getRequestDispatcher("/login").forward(request, response);
+			return;
+		}
 
-	} catch (NoSuchUserException | NoSuchRoleException e) {
-	    request.setAttribute("loginerror", e.getMessage());
-	    request.getRequestDispatcher("/login")
-		    .forward(request, response);
-	    return;
-	} catch (SQLException e) {
-	    // TODO REDIRECT NA "SORRY PAGE"
-	    LOGGER.error("Exception during users authorization.");
-	    return;
+		User user;
+		ArrayList<Permission> permissions;
+		HttpSession session = request.getSession();
+		try {
+			user = authorizationService.getUser(login, password);
+
+			if (user == null) {
+				throw new NoSuchUserException();
+			}
+
+			permissions = permissionService.getPermissions(user.getIdRole());
+			session.setAttribute("user", user);
+			session.setAttribute("permissions", permissions);
+
+			// request.getRequestDispatcher("/mainjsp").forward(request,
+			// response);
+			LOGGER.info("User " + user.getLogin() + "(id:" + user.getId()
+					+ ") logged in. ");
+			response.sendRedirect("main");
+
+		} catch (NoSuchUserException | NoSuchRoleException e) {
+			request.setAttribute("loginerror", e.getMessage());
+			request.getRequestDispatcher("/login").forward(request, response);
+			return;
+		} catch (SQLException e) {
+			// TODO REDIRECT NA "SORRY PAGE"
+			LOGGER.error("Exception during users authorization.");
+			return;
+		}
 	}
-    }
 
-    private boolean validate(String login, String password) {
-	if (login == null || password == null || login == "" || password == "") {
-	    return false;
+	private boolean validate(String login, String password) {
+		if (login == null || password == null || login == "" || password == "") {
+			return false;
+		}
+		return true;
 	}
-	return true;
-    }
 }
