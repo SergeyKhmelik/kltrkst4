a753a558f4d0d018d621f215c5af66aa79f10758
Alex Babenko <alex0128.94@mail.ru>
2015-02-01 15:05:15 +0200

    tests restored

---
+++ SummaryTask4/src/test/java/ua/nure/khmelik/SummaryTask4/entity/JournalTest.java
@@ -0,0 +1,14 @@
+package ua.nure.khmelik.SummaryTask4.entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JournalTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
